58cd1b725bd38cd3071061eae9ffeaac3cc89010
mayorquinmachines <smayorquin@berkeley.edu>
2017-05-28 10:39:02 -0400

    Added setup script for application

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ def readme():
         return f.read()
 
 setup(
-    name='Apt Hunt',
+    name='AptHunt',
     version='0.1',
     description='Swipe interface for smart filtered apartment hunting',
-    url='https://github.com/TrrRdrgz/apt_hunt',
+    url='https://github.com/mayorquinmachines/AptHunt',
     license='MIT',
     long_description=readme(),
     install_requires=["boto3", "python-crontab", "geopy", "lxml", "requests", "simplejson"]
---
+++ setup_app.py
@@ -0,0 +1,20 @@
+def main():
+    import subprocess
+    import os
+    install_lst = ['apache2', 'update', 'libapache2-mod-wsgi', 'python-flask', 'upgrade']
+    print "Installing dependencies"
+    for package in install_lst:
+        cmd = 'sudo apt-get install '+ package
+        subprocess.call(cmd.split(), shell=True)
+    print "Moving directories and files"
+    Flask_path = os.path.abspath('FlaskApps/')
+    os.rename(Flask_path, '/var/www/FlaskApps')
+    os.rename('/var/www/FlaskApps/AptHunt.conf', '/etc/apache2/sites-available/AptHunt.conf')
+    setup_cmd = [['a2enmod', 'wsgi'], ['apachectl', 'restart'], ['a2ensite', 'AptHunt'], ['service', 'apache2',
+    'reload'], ['/etc/init.d/apache2/reload'], ['service', 'apache2', 'restart'], ['/etc/init.d/apache2/reload']]
+    for cmd in install_lst:
+        subprocess,call(['sudo']+cmd, shell=True)
+    print "Done!"
+
+if __name__ == "__main__":
+  main()
