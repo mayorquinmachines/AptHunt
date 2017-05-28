99bd4f68ef76981b9a3f663565fbe2674d5d5e27
mayorquinmachines <smayorquin@berkeley.edu>
2017-05-28 11:53:15 -0400

    Added setup shell script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ sudo apt-get update
 sudo apt-get install libapache2-mod-wsgi
 sudo apt-get install python-flask
 sudo apt-get upgrade
-sudo apt-get python-pip
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
 
 sudo pip install boto3
 sudo pip install python-crontab
@@ -15,7 +16,7 @@ sudo pip install lxml
 sudo pip install requests
 sudo pip install simplejson
 
-sudo mv FlaskApps/ /var/www/FlaskApps
+sudo mv ./FlaskApps/ /var/www/FlaskApps
 sudo mv /var/www/FlaskApps/AptHunt.conf /etc/apache2/sites-available/AptHunt.conf
 
 sudo a2enmod wsgi
@@ -25,5 +26,4 @@ sudo service apache2 reload
 sudo /etc/init.d/apache2 reload
 
 sudo service apache2 restart
-/etc/init.d/apache2 reload
-
+sudo /etc/init.d/apache2 reload
