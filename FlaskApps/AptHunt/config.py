867c4c939384b001a20659f40654d5758d13d3a0
mayorquinmachines <smayorquin@berkeley.edu>
2017-07-05 15:01:35 -0400

    added empty database and updated config.py

--- FlaskApps/AptHunt/config.py
+++ FlaskApps/AptHunt/config.py
@@ -2,16 +2,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 from datetime import timedelta
 
+WTF_CSRF_ENABLED = True
 
-DEBUG = True
+
 SECRET_KEY = 'yoursecretkey'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SECURITY_REGISTERABLE = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' 
 DEFAULT_MAIL_SENDER = 'yourgmailaddress@gmail.com'
 SECURITY_PASSWORD_HASH = 'bcrypt'
 SECURITY_PASSWORD_SALT = 'salty'
 SECURITY_TRACKABLE = True
-SEND_REGISTER_EMAIL = False
+SECURITY_CONFIRMABLE = True
 SECURITY_RECOVERABLE = True
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 465
